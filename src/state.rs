88eaf333914f3cf0bfecc793036d118bc443538f
Geoffroy Couprie <geo.couprie@gmail.com>
2017-07-01 19:13:38 +0200

    carry the context in a struct

--- src/state.rs
+++ src/state.rs
@@ -5,8 +5,7 @@ use parser::{self,block,header,Block};
 pub enum State {
     Initial,
     Error,
-    // file_size, file offset, list
-    Blocks(usize, usize, List),
+    Blocks(Context),
 }
 
 #[derive(Debug,Clone,PartialEq)]
@@ -16,10 +15,17 @@ pub enum List {
     Node(usize, parser::List, Box<List>),
 }
 
+#[derive(Debug,Clone,PartialEq)]
+pub struct Context {
+    file_size:     usize,
+    stream_offset: usize,
+    level:         List,
+}
+
 pub fn advance(state: State, input: &[u8]) -> (usize, State) {
     match state {
         State::Initial => parse_initial(input),
-        State::Blocks(file_size, offset, level) => parse_blocks(input, file_size, offset, level),
+        State::Blocks(context) => parse_blocks(input, context),
         _              => panic!("unimplemented state"),
 
 
@@ -31,46 +37,56 @@ pub fn parse_initial(input: &[u8]) -> (usize, State) {
     match header(input) {
         IResult::Error(_)        => (0, State::Error),
         IResult::Incomplete(_)   => (0, State::Initial),
-        IResult::Done(i, header) => (input.offset(i), State::Blocks(header.file_size as usize, input.offset(i), List::Nil)),
+        IResult::Done(i, header) => (input.offset(i), State::Blocks(Context {
+            file_size: header.file_size as usize,
+            stream_offset: input.offset(i),
+            level: List::Nil
+        })),
     }
 }
 
-pub fn parse_blocks(input: &[u8], file_size: usize, offset: usize, level: List) -> (usize, State) {
+pub fn parse_blocks(input: &[u8], mut ctx: Context) -> (usize, State) {
     //FIXME: handle closing list
-    let sl = match level {
+    let sl = match ctx.level {
         List::Nil                   => input,
-        List::Node(remaining, _, _) => &input[..(remaining - offset)]
+      // min of offset and input length?
+        List::Node(remaining, _, _) => &input[..(remaining - ctx.stream_offset)]
     };
 
-    match block(sl, offset, file_size as u32) {
-        IResult::Error(_)        => (0, State::Error),
-        IResult::Incomplete(_)   => (0, State::Blocks(file_size, offset, level)),
+    match block(sl, ctx.stream_offset, ctx.file_size as u32) {
+        IResult::Error(e)        => {
+          println!("got error: {:?}", e);
+          (0, State::Error)
+        },
+        IResult::Incomplete(_)   => (0, State::Blocks(ctx)),
         IResult::Done(i, blk) => {
-            let remaining_offset = input.offset(i);
+            let advancing = input.offset(i);
+            ctx.stream_offset += advancing;
             match blk {
-                Block::Unimplemented => panic!("unimplemented block:\n{}", &input[..input.offset(i)].to_hex(16)),
-                Block::Default       => panic!("default block:\n{}", &input[..input.offset(i)].to_hex(16)),
+                Block::Unimplemented => panic!("unimplemented block:\n{}", &input[..advancing].to_hex(16)),
+                Block::Default       => panic!("default block:\n{}", &input[..advancing].to_hex(16)),
                 Block::Avih(h)       => {
                     println!("got main AVI header: {:?}", h);
-                    (remaining_offset, State::Blocks(file_size, offset + remaining_offset, level))
+                    (advancing, State::Blocks(ctx))
                 },
                 Block::List(size, l) => {
-                    match level {
-                        List::Nil => (remaining_offset,
-                            State::Blocks(
-                                file_size,
-                                offset + remaining_offset,
-                                List::Node(offset + remaining_offset + size, l, Box::new(List::Nil)))),
+                    match ctx.level {
+                        List::Nil => (advancing,
+                            State::Blocks(Context {
+                                file_size: ctx.file_size,
+                                stream_offset: ctx.stream_offset,
+                                level: List::Node(ctx.stream_offset + size, l, Box::new(List::Nil))
+                            })),
                         List::Node(sz, _, _) => {
-                            if sz < offset + remaining_offset + size {
+                            if sz < ctx.stream_offset + size {
                                 // the new list would be larger than the parent one
-                                (remaining_offset, State::Error)
+                                println!("the new list would be larger ({}) than the parent one ({})",
+                                    ctx.stream_offset + size, sz);
+                                (advancing, State::Error)
                             } else {
-                                (remaining_offset,
-                                State::Blocks(
-                                    file_size,
-                                    offset + remaining_offset,
-                                    List::Node(offset + remaining_offset + size, l, Box::new(level))))
+                                ctx.level = List::Node(ctx.stream_offset + size, l, Box::new(ctx.level));
+                                (advancing,
+                                State::Blocks(ctx))
                             }
                         }
                     }
